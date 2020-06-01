8f5b48036538fced193784c3704733e3d48f9b0e
Nouman <mrnoumanbaloch@gmail.com>
2020-06-01 14:22:03 +0500

    Comments

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const frontendRouter = require('./routers/frontendRouter');
 const moderatorSchema = require('./models/moderatorModels');
 const app = express();
 
+//This body parser is use to get the values from body request of submit and article form page and make a post call.
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 //middlewares
@@ -28,24 +29,38 @@ app.use((req, res, next) => {
 //Post request to submit article
 app.post('/api/v1/article/moderator', urlencodedParser, function(req, res, next){
   //Post request for an article.
+  //Condition run if this is article.
   if(req.body.choice == "isArticle"){
+    //This post object is created which store the request body from article page in object.
     var post = new moderatorSchema({
+      //Here body request contain the input data of user and give it to title and detail properties.
       title: req.body.title,
+      //Concatenation of user input
       detail: req.body.author + '. (' + req.body.year + '). ' + req.body.title + '. ' + req.body.journel + ', ' + req.body.volume + ', ' + req.body.startPage + '-' + req.body.endPage + '. ' + req.body.DOI + '.',
     })
+    //save method save the data into API end point of /api/v1/article/moderator
     post.save(function (err, post) {
+      //In case error occur
       if (err) { return next(err) }
+      //After successful submission user will be redirected to submit book page.
       res.redirect('/submit-article');
     })
   }
   //Post request for a book.
+  //Condition run if this is book.
   if(req.body.choice == "isBook"){
+    //This post object is created which store the request body from book page in object.
     var post = new moderatorSchema({
+      //Here body request contain the input data of user and give it to title and detail properties.
       title: req.body.title,
+      //Concatenation of user input
       detail: req.body.author + '. (' + req.body.year + '). ' + req.body.title + '. ' + req.body.place + ': ' + req.body.name +'.',
     })
+    //save method save the data into API end point of /api/v1/article/moderator
     post.save(function (err, post) {
+      //In case error occur
       if (err) { return next(err) }
+      //After successful submission user will be redirected to submit book page.
       res.redirect('/submit-book');
     })
   }
